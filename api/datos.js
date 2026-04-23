0d54db07cf6001eb65c338215e1755277fbd115d
academiafutbolcto <academiafutbol.cto@gmail.com>
2026-04-23 14:10:24 +0200

    Add files via upload

---
+++ api/datos.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+    const { id } = req.query;
+
+    try {
+        const response = await fetch(`https://api.jsonbin.io/v3/b/${id}/latest`, {
+            headers: {
+                "X-Access-Key": process.env.JSONBIN_KEY
+            }
+        });
+
+        const data = await response.json();
+
+        res.status(200).json(data.record);
+
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener datos" });
+    }
+}
